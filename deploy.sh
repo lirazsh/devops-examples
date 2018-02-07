f489a92111a15f3e6cea21d2b0a24cc1a78c44e3
Liraz <liraz.sharaby@kaltura.com>
2018-02-07 21:02:06 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 yum update -y
 yum install epel-release -y
-yum install pip -y
+yum install python-pip -y
 
-pip install --upgrade pip -y
+pip install --upgrade pip
 
-pip install ansible -y
+pip install ansible
 
 yum install git -y
 
-mkdir /etc/ansible
+#mkdir /etc/ansible
 
-git init /etc/ansible
+#git init /etc/ansible
 
 git clone http://github.com/lirazsh/devops-examples/ /etc/ansible
 
@@ -20,7 +20,7 @@ ssh-keygen -P "" -f /root/.ssh/id_rsa
 
 mkdir /root/.ssh/authorized_keys
 
-cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys/localhost.pub
 
 ssh-copy-id localhost
 
